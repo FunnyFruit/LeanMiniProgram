d4f880cbcfcc80b00409b0f9178a0305be2243fd
FunnyFruit <1576416275@qq.com>
2019-11-26 17:00:02 +0800

    12_事件处理

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,62 +5,76 @@ Page({
    * 页面的初始数据
    */
   data: {
+    titles:['鞋子','衣服','裤子','滑稽']
+
+  },
+  handleBtnClick(){
+    console.log("卧槽 不要点我");
+  },
+
+  handleToucheStart(){
+    console.log('handleToucheStart')
+  },
+
+  handleToucheMove() {
+    console.log('handleToucheMove')
+  },
+  handleTouchend() {
+    console.log('handleTouchend')
+  },
+  handleTap() {
+    console.log('handleTap')
+  },
+  handleLongpress() {
+    console.log('handleLongpress')
+  },
+
+  handleEventClick(event){
+      console.log('=====',event)
+  },
+  handleEnentEnd(event){
+    console.log("+++++",event)
+
+  },
+  
+   handleInner(event) {
+    console.log("内层的view", event)
+  },
+  
+  
+   handleOuter(event) {
+    console.log("外层的view", event)
+  },
+
+  handleItemClick(event) {
+    console.log('----', event)
+    const dataset = event.currentTarget.dataset
+    const title = dataset.item
+    const index = dataset.index
+    console.log(title,index)
+},
+// 事件冒泡和事件的捕获
+  handleCaptureView1(){
+    console.log("handleCaptureView1")
+  },
+  handleBindView1(){
+    console.log("handleBindView1")
+  },
+  handleCaptureView2() {
+    console.log("handleCaptureView2")
+
+  },
+  handleBindView2() {
+    console.log("handleBindView2")
+
+  }, handleCaptureView3() {
+    console.log("handleCaptureView3")
+
+  },
+  handleBindView3() {
+    console.log("handleBindView3")
 
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
 
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
